01040b03d0520e5911826347ce89d222911624ce
MaliiqAshmeade <maliiqa@nycstudents.net>
2025-09-11 15:35:09 -0400

    added return food

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         Feeder f = new Feeder();
+        System.out.println(f);
+        System.out.println(f.getCurrentFood());
     }
 }
